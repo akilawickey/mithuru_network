e0462f64b5d64985f901c76282bb98cb300fd30f
Akila Wickey <asak999@gmail.com>
2016-02-07 21:48:28 +0530

    Update connect.inc.php

--- connect.inc.php
+++ connect.inc.php
@@ -1,11 +1,11 @@
 <?php
 
-$mysql_host = 'localhost';
-$mysql_user = "root";
-$mysql_password = "root";
+$mysql_host = 'web: vendor/bin/heroku-php-apache2 web/';
+$mysql_user = "tozhlmnpgxjaor";
+$mysql_password = "hDUUia5L8pn2pC0vyAfRLIDVWD";
 
 
-$mysql_db = 'a3660097_danny';
+$mysql_db = 'dcesfckjv2jpvh';
 
 
 if(!mysql_connect($mysql_host,$mysql_user,$mysql_password) || !mysql_select_db($mysql_db)){
